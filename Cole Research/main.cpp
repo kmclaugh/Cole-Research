45191683c399599fee51b3794e0735f580cda995
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2013-01-15 13:07:09 -0500

    Initial Commit

---
+++ Cole Research/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Cole Research
+//
+//  Created by Kevin McLaughlin on 1/15/13.
+//  Copyright (c) 2013 Cole-McLaughlin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
