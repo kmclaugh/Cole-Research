16f3f490cfb9273b72a78666a2ca429f71a13bdb
Kevin McLaughlin <kmclaugh@bu.edu>
2013-12-23 12:34:27 -0500

    updating

---
+++ python_orbits_graphs.py
@@ -0,0 +1,83 @@
+from math import *
+from numpy import *
+from matplotlib import *
+from pylab import *
+import mpl_toolkits.mplot3d.axes3d
+import datetime
+import os
+
+current_directory = os.getcwd()
+plots_directory = current_directory+"/Plots"
+current_data_directory = current_directory+"/Current_Data"
+
+## Convets a file with lines formated as "xvalue, yvalue\n" to a list of x_values and a list of y_values. Returns the tuple (x_values,y_values)
+def comma_seperated_file_to_xy_list(filename):
+    x_vals = []
+    y_vals = []
+    the_file = open(filename,"r")
+    for line in the_file:
+        split = line.split(",")
+        try:
+            x_value = float(split[0].replace(" ",""))
+            y_value = float(split[1].replace(" ",""))
+            x_vals.append(x_value)
+            y_vals.append(y_value)
+        except:
+            pass
+    return((x_vals,y_vals))
+
+## Converts a file with lines format as "value1, value2, value3..., valuen\n" to a plot with the formating particulars given in the function. Saves the plot in the /Plots directory as "PLOT_TITLE-CURRENT_TIME.pdf" to ensure graphs are not accidently overwritten. NOTE still does not work for more than two values.
+def comma_seperated_file_to_graph(filename,the_xlabel="x",the_ylabel="y",the_zlabel=None, the_title="title",marker=None,linestyle="-",color="b"):
+    current_date_and_time = datetime.datetime.now()
+    the_file = open(filename,"r")
+    for line in the_file:
+        split_values = line.split(",")
+        break
+    values = []
+    for one_values in split_values:
+        values.append([])
+    for line in the_file:
+        counter = 0
+        split_values = line.split(",")
+        for string_value in split_values:
+            float_value = float(string_value.replace(" ",""))
+            values[counter].append(float_value)
+            counter += 1
+    if len(values) == 2:
+        plot(values[0], values[1], marker=marker, linestyle=linestyle, color=color)
+        ylabel(the_ylabel)
+        xlabel(the_xlabel)
+        title(the_title)
+        todays_date = datetime.datetime.now()
+        savefig(plots_directory+"/{}-{}.pdf".format(the_title,current_date_and_time), bbox_inches=0)
+    if len(values) == 3:
+        print("more than 3 values doesn't work yet")
+#        fig=figure()
+#        ax = mpl_toolkits.mplot3d.axes3d.Axes3D(fig)
+#        ax.plot_surface(values[0],values[1],values[2])
+#        ax.set_xlabel('X')
+#        ax.set_ylabel('Y')
+#        ax.set_zlabel('Z')
+#        show()
+
+## Note that you must move the .dat files created by the multi_elliptical file to the /Current_Data directory after running multi_elliptical simulation. This is to prevent accencidental overwrite of .dat files as they take a long time to generate and the multi_elliptical will automatically overwrite previous files.
+
+## Eccentricity
+eccentricity_filename = current_data_directory+"/eccen.dat"
+comma_seperated_file_to_graph(eccentricity_filename,the_xlabel="x",the_ylabel="eccentricity",the_title="Eccentricity")
+
+## Semimajor
+semimajor_filename = current_data_directory+"/smajor.dat"
+comma_seperated_file_to_graph(semimajor_filename,the_xlabel="x",the_ylabel="semimajor axis",the_title="Semimajor Axis")
+
+## Theta0
+theta0_filename = current_data_directory+"/theta0.dat"
+comma_seperated_file_to_graph(theta0_filename,the_xlabel="x",the_ylabel="theta0",the_title="Theta0")
+
+## Orbit
+#orbit_filename = current_data_directory+"/orbit.dat"
+#comma_seperated_file_to_graph(orbit_filename,the_title="orbit")
+
+
+
+
